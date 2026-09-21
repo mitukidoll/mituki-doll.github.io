d40b737269c1dbe139229c69c0a0b77fd8af7b16
mitukidoll <152673183+mitukidoll@users.noreply.github.com>
2026-09-21 10:28:01 +0900

    ギャラリー追加

--- script.js
+++ script.js
@@ -44,3 +44,15 @@ setTimeout(() => {
     requestAnimationFrame(updateGlitch);
 
 }, 10000);
+
+const slides = document.querySelectorAll(".gallery-slide");
+
+let currentSlide = 0;
+
+setInterval(() => {
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide = (currentSlide + 1) % slides.length;
+
+    slides[currentSlide].classList.add("active");
+}, 3000);
